ae37c322c1d64afaa7640006a3096801f8945ac0
itz_amirsadra <sadra.etd8456@gmail.com>
2021-11-02 23:23:51 +0330

    update files

--- JS/main.js
+++ JS/main.js
@@ -48,11 +48,9 @@ function switchTheme(e) {
   if (e.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
     localStorage.setItem("theme", "dark");
-    body.style.backgroundImage = "url(/img/background-2.jpg)"
   } else {
     document.documentElement.setAttribute("data-theme", "light");
     localStorage.setItem("theme", "light");
-    body.style.backgroundImage = "url(/img/background-1.jpg)"
   }
 }
 
